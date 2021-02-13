0605af3594a5eb00e7a7db9942b70f2efbaac8ff
ArtBro <artem.borsh95@gmail.com>
2021-02-13 17:30:43 +0300

    index page desktop

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -3,10 +3,43 @@ let sliderMain = document.querySelector('.slider-main');
 let slider = new Swiper('.slider-main', {
 	pagination: {
 		el: '.slider-main__pagination',
-		clickable: true,
-		// renderBullet: function (index, className) {
-		// 	return `<span class="` + className + `"><svg><circle cx="8" cy="8" r="6" style="stroke-dashoffset: calc(37.7 - (37.7 * ${(index + 1) / countSlide * 100}) / 100);
-		// "></circle></svg></span>`;
-		// }
+		clickable: true
 	},
 });
+
+
+
+//Кнопка "Наверх"
+const btnDown = document.querySelector('.v-up');
+
+vUp(btnDown, scroledWindow);
+
+btnDown.addEventListener('click', function () {
+	console.log(23)
+	backToTop();
+});
+
+window.addEventListener('scroll', () => {
+	vUp(btnDown, scroledWindow);
+});
+
+function vUp(btn, scroled, scrollTop) {
+
+	if (scroled() > (document.documentElement.clientHeight / 2)) {
+		btn.classList.add('active');
+	} else if (scroled() < (document.documentElement.clientHeight / 2) || btn.classList.contains('active')) {
+		btn.classList.remove('active');
+	}
+}
+
+function scroledWindow() {
+	return window.pageYOffset || document.documentElement.scrollTop;
+}
+
+function backToTop() {
+	if (window.pageYOffset <= 0) return;
+
+	window.scrollBy(0, -80);
+	setTimeout(backToTop, 0);
+
+}
